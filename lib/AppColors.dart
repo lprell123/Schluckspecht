bdaffd286b3cb5f11adc62f774218d50e4da8e0b
SiraWeiner <Sira_weiner@web.de>
2023-12-12 18:46:52 +0100

    Alle Änderungen bis jetzt

---
+++ lib/AppColors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color Primaryblue = Color(0xFF141C6C);
+  static Color SecondaryBlue = Color(0xFF00c2c9);
+  static Color PrimaryRed =Color(0xFFfc0404); 
+  static Color SecondaryGrey =Colors.grey[200]!; 
+}
