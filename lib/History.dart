889976c181e9721de7e3220b538313034f81b252
kaulquabbe <joshua.gargano@pm.me>
2024-01-21 13:42:02 +0100

    Tags (1/2)

--- lib/History.dart
+++ lib/History.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import "package:schluckspecht_app/componentsTimeline/my_timeline_tile.dart";
@@ -7,20 +8,56 @@ import 'package:schluckspecht_app/mycustomappbar.dart';
 import 'package:http/http.dart' as http;
 import 'AppThemes.dart';
 import 'ErrorCard.dart';
+import 'tags.dart';
 
 
 class Historypage extends StatelessWidget {
-  Historypage({super.key});
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  const Historypage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return Timeline();
+  }
+  
+
+  
+}
+
+class Timeline extends StatefulWidget {
+  const Timeline({super.key});
+
+  @override
+  State<Timeline> createState() => _Timeline();
+
+}
+
+class _Timeline extends State<Timeline> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       appBar: MyAppBar(title: 'Historie', scaffoldKey: scaffoldKey),
       drawer: MyDrawer(),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.cardColor,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (_) {
+              return PopupDialog();
+            }
+          );
+        },
+        child: const Icon(Icons.filter_alt),
+      ),
+
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        padding: AppCardStyle.innerPadding,
         child: FutureBuilder(
           future: fetchData(),
           builder: (context, data) {
@@ -66,8 +103,8 @@ class Historypage extends StatelessWidget {
       )
     );
   }
-}
 
+}
 
 Future<List<Events>> fetchData() async {
   try {
@@ -174,7 +211,7 @@ class FullText extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        padding: AppCardStyle.innerPadding,
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -187,7 +224,7 @@ class FullText extends StatelessWidget {
                   //DATUM
                   Text(
                     "$day / $month / $year",
-                    style: const TextStyle(fontSize: 16),
+                    style: const TextStyle(fontSize: AppTextStyle.regularFontSize),
                   ),
                 ],
               ),
@@ -199,7 +236,7 @@ class FullText extends StatelessWidget {
                   Text(
                     event.placement ?? "",
                     style: const TextStyle(
-                      fontSize: 28,
+                      fontSize: AppTextStyle.titleSize,
                       fontWeight: FontWeight.bold,
                     ),
                     overflow: TextOverflow.visible,
@@ -209,9 +246,9 @@ class FullText extends StatelessWidget {
                   Text(
                     event.title ?? "",
                     style: const TextStyle(
-                      fontSize: 24,
+                      fontSize: AppTextStyle.largeFontSize,
                       fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 131, 131, 131),
+                      color: AppColors.accentFontColor,
                     ),
                     overflow: TextOverflow.visible,
                   ),
@@ -229,10 +266,10 @@ class FullText extends StatelessWidget {
                 ),
               const SizedBox(height: 16),
               Padding(
-                padding: const EdgeInsets.only(bottom: 24.0),
+                padding: const EdgeInsets.only(bottom: 15.0),
                 child: Text(
                   event.content ?? "",
-                  style: const TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: AppTextStyle.largeFontSize),
                   overflow: TextOverflow.visible,
                 ),
               ),
@@ -244,3 +281,57 @@ class FullText extends StatelessWidget {
   }
 }
 
+class PopupDialog extends StatefulWidget {
+  @override
+  _PopupDialog createState() => new _PopupDialog();
+}
+
+class _PopupDialog extends State<PopupDialog> {
+
+
+  @override 
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Nach Tags filtern'),
+      surfaceTintColor: AppColors.cardColor,
+      content: StatefulBuilder(
+        builder: (context, setState) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+                const SizedBox(height: 8.0),
+                Wrap(
+                  spacing: 8.0,
+                  children: Tags.tags.map((tag) {
+                    return FilterChip(
+                      backgroundColor: AppColors.secondaryGrey,
+                      selectedColor: AppColors.primaryBlue,
+                      showCheckmark: false,
+                      shape: RoundedRectangleBorder(borderRadius: AppCardStyle.cardBorderRadius),
+                      label: Text(tag, 
+                        style: const TextStyle(
+                          fontSize: AppTextStyle.regularFontSize,
+                          color: AppColors.primaryFontColor,
+                        ),
+                      ),
+                      selected: Tags.selectedTags.contains(tag),
+                      onSelected: (selected) {
+                        setState(() {
+                          if(selected) { 
+                            Tags.selectedTags.add(tag);
+                          } else {
+                            Tags.selectedTags.remove(tag);
+                          }
+                        });
+                      }
+                    );
+                  }).toList(),
+                ),
+            ],
+          );
+        }
+      ),
+    );
+  }
+}
--- lib/componentsTimeline/date_card.dart
+++ lib/componentsTimeline/date_card.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:schluckspecht_app/AppThemes.dart";
 
 class DateCard extends StatelessWidget {
   int? day;
@@ -10,13 +11,13 @@ class DateCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
+      margin: AppCardStyle.cardMargin,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          Text("$year", style: const TextStyle(color: Colors.grey, fontSize: 10,) ),
-          Text("$day / $month", style: const TextStyle(color: Color.fromARGB(255, 36, 36, 36), fontSize: 11,) ),
+          Text("$year", style: const TextStyle(color: AppColors.secondaryFontColor, fontSize: AppTextStyle.smallFontSize,) ),
+          Text("$day/$month", style: const TextStyle(color: AppColors.primaryFontColor, fontSize: AppTextStyle.smallFontSize) ),
         ],
       ),
     );
